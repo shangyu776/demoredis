459ac5a03c49c9b1c245280c96a4d9ae39294179
shangyu776 <shangyuvip@126.com>
2022-03-22 08:46:55 +0800

    测试推送

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -16,8 +16,8 @@ import java.util.List;
  * 前端控制器
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
+ * @author yu
+ * @since 2022-03-21
  */
 @RestController
 @RequestMapping("/shop-type")
@@ -29,6 +29,6 @@ public class ShopTypeController {
     public Result queryTypeList() {
         List<ShopType> typeList = typeService
                 .query().orderByAsc("sort").list();
-        return Result.ok("builder");
+        return Result.ok("buildertetst");
     }
 }
